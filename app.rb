0036a82559dbb3492f03e67b0ea62725bc7f60a5
casselsg <casselsg19@bcdschool.org>
2018-07-20 19:27:54 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,16 +1,28 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'views'
+  end
+
+  get '/' do
+    "This is a homepage"
+  end 
+
   get '/reverse' do
     erb :reverse
   end
+  
+  post '/reverse' do 
+  original_string = params["string"]
+  @reversed_string = original_string.reverse
+    erb :reversed 
+  end 
+  
+  get '/friends' do 
+     @friends 
+  end 
 
-  post '/reverse' do
-    erb :reversed
-  end
-
-  get '/friends' do
-    # Write your code here!
-
-  end
 end
